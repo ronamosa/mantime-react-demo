8eb61ca27bb62a22779c373636a54ba688799cbc
Ron <32425521+ronamosa@users.noreply.github.com>
2022-08-21 00:10:40 +1200

    initial setup
    added components header, footer, ringstats
    yarn added missing dependencies tabler/icons, mantine/ds

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import { MantineProvider, Text, Button, Stack } from "@mantine/core";
 import { theme } from "./theme";
+import { FooterSimple } from "./components/FooterSimple"
+import { HeaderMenu } from "./components/HeaderMenu"
+import { StatsRingCard } from "./components/StatsRingCard"
 
 export default function App() {
   return (
     <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+      <HeaderMenu links={[]} />
       <Stack align="center" mt={50}>
         <Text size="xl" weight={500}>
-          Welcome to Mantine!
+          Microservices Dashboard
         </Text>
-        <Button>Click the button</Button>
+        <Button>Health Check</Button>
+        <StatsRingCard title={"Services"} completed={4} total={5} stats={[
+          {
+            "value": 20,
+            "label": "Healthy"
+          },
+          {
+            "value": 5,
+            "label": "Pending"
+          }
+        ]} />
       </Stack>
+      <FooterSimple links={[]}/>
     </MantineProvider>
   );
 }
